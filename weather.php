e1bc1dabe9862cd6b7dbded9aa42a74cea21a207
Anthony Fletcher <afletcher2433.com>
2023-11-29 17:49:08 -0500

    updated classes

--- weather.php
+++ weather.php
@@ -15,6 +15,35 @@ class Weather extends Model {
         'humidity', 
         'wind_speed',
         'conditions', 
+    ]; 
+
+    protected $casts = [ 
+        'location' => [
+            'type' => 'string',
+            'length' => 200,
+            'nullable' = false,
+        ],
+
+        'temperature' => [
+            'type' => 'integer',
+            'nullable' => false,
+        ],
+
+        'humidity' => [
+            'type' => 'integer',
+            'nullable' => false,
+        ],
+
+        'wind_speed' => [
+            'type' => 'integer',
+            'nullable' => false,
+        ],
+
+        'conditions' => [
+            'type' => 'string',
+            'length' => 250,
+            'nullable' => false,
+        ]
     ];
 
     public function __construct(array $attributes = []) {
@@ -49,6 +78,21 @@ class User extends Model {
         'password',
     ];
 
+    protected $casts = [
+        'email' => [
+            'type' => 'string',
+            'length' => 50,
+            'unique' => true,
+            'nullable' => false,
+        ],
+
+        'password' => [
+            'type' => 'string',
+            'length' => 150,
+            'nullable' => true,
+        ]
+    ];
+
     public function __construct(array $attributes = []) {
         parent::__construct($attributes);
     }
@@ -66,7 +110,28 @@ class Account extends Model {
     protected $fillable = [
         'email',
         'password',
-    ]
+        'user_id',
+    ];
+
+    protected $casts = [
+        'email' => [
+            'type' => 'string', 
+            'length' => 50,
+            'unique' => true,
+            'nullable' => false,
+        ],
+
+        'password' => [ 
+            'type' => 'string',
+            'length' => 150,
+            'nullable' => true,
+        ],
+
+        'user_id' => [
+            'type' => 'integer',
+            'nullable' => false,
+        ]
+    ];
 
     public function __construct(array $attributes = []) {
         parent::__construct($attributes); 
@@ -74,6 +139,91 @@ class Account extends Model {
 
     public function __toString() {
         return sprintf('<User id=%s account=%s', $this->id, $this->account);
+    }
+}
+
+class City extends Model {
+    protected $table = 'cities';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'city',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'city' => [
+            'type' => 'integer',
+            'nullable' => true,
+        ],
+
+        'user_id' => [
+            'type' => 'integer',
+            'nullable' => false, 
+        ]
+    ];
+
+    public function __construct(array $attributes = []) {
+        parent::__construct($attributes); 
+    } 
+
+    public function __toString() { 
+        return sprintf('<City id=%s city=%s user_id=%s', $this->id, $this->city, $this->user_id);
+    }
+}
+
+class Country extends Model {
+    protected $table = 'countries';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'country',
+        'city',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'country' => [
+            'type' => 'string',
+            'length' => 100,
+            'nullable' = false,
+        ]
+        
+        'city' => [
+            'type' = 'string',
+            'length' = 100,
+            'nullable' = false,
+        ]
+    ]
+}
+
+class Favorite extends Model {
+    protected $table = 'favorites';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'city',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'city' => [
+            'type' => 'string',
+            'length' => 100,
+            'nullable' => true,
+        ],
+
+        'user_id' => [
+            'type' => 'integer',
+            'nullable' => false,
+        ]
+    ];
+
+    public function __construct(array $attributes = []) {
+        parent::__construct($attributes); 
     }
 }
 
